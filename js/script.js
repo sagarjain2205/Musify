3a7ed339d6a13c4280251269016e60b55a237d2d
sagarjain2205 <jainsagar22105@gmail.com>
2026-03-06 13:42:36 +0530

    final fix for vercel music loading

--- js/script.js
+++ js/script.js
@@ -5,10 +5,10 @@ let songs = [];
 let currFolder;
 let currentIndex = 0;
 
-// Album folders (ADD HERE if you create new folder)
+// Album folders
 let albumFolders = [
-  "seedhe_maut" ,
-  "krsna" ,
+  "seedhe_maut",
+  "krsna",
   "Diljit",
   "karan_aujla",
   "Angry_(mood)",
@@ -28,26 +28,15 @@ function secondsToMinutesSeconds(seconds) {
   return `${String(minutes).padStart(2,"0")}:${String(remainingSeconds).padStart(2,"0")}`;
 }
 
+// 🔥 FIXED FUNCTION (NOW USES info.json)
 async function getSongs(folder) {
+
   currFolder = folder;
 
-  let response = await fetch(`songs/${folder}/`);
-  let text = await response.text();
+  let response = await fetch(`songs/${folder}/info.json`);
+  let data = await response.json();
 
-  let div = document.createElement("div");
-  div.innerHTML = text;
-
-  let anchors = div.getElementsByTagName("a");
-  songs = [];
-
-  for (let anchor of anchors) {
-    if (anchor.href.endsWith(".mp3")) {
-      let songName = decodeURIComponent(
-        anchor.href.split(`songs/${folder}/`)[1]
-      );
-      songs.push(songName);
-    }
-  }
+  songs = data.songs;
 
   let songUL = document.querySelector(".songList ul");
   songUL.innerHTML = "";
@@ -57,7 +46,7 @@ async function getSongs(folder) {
       <li>
         <img class="invert" width="34" src="img/music.svg">
         <div class="info">
-          <div>${song}</div>
+          <div>${song.replace(".mp3","")}</div>
           <div>Sagar</div>
         </div>
       </li>`;
@@ -73,7 +62,9 @@ async function getSongs(folder) {
 }
 
 function playMusic(track, pause = false) {
+
   currentIndex = songs.indexOf(track);
+
   currentSong.src = `songs/${currFolder}/` + track;
 
   if (!pause) {
@@ -81,15 +72,18 @@ function playMusic(track, pause = false) {
     play.src = "img/pause.svg";
   }
 
-  document.querySelector(".songinfo").innerHTML = track;
+  document.querySelector(".songinfo").innerHTML = track.replace(".mp3","");
 }
 
 async function displayAlbums() {
+
   let cardContainer = document.querySelector(".cardContainer");
   cardContainer.innerHTML = "";
 
   for (let folder of albumFolders) {
+
     try {
+
       let res = await fetch(`songs/${folder}/info.json`);
       let data = await res.json();
 
@@ -100,56 +94,86 @@ async function displayAlbums() {
           <h2>${data.title}</h2>
           <p>${data.description}</p>
         </div>`;
+
     } catch (err) {
+
       console.log("Skipping album:", folder);
+
     }
   }
 
   Array.from(document.getElementsByClassName("card")).forEach(card => {
+
     card.addEventListener("click", async (e) => {
+
       let folder = e.currentTarget.dataset.folder;
+
       await getSongs(folder);
+
       if (songs.length > 0) playMusic(songs[0]);
+
     });
+
   });
+
 }
 
 async function main() {
+
   await displayAlbums();
+
   await getSongs("ncs");
+
   if (songs.length > 0) playMusic(songs[0], true);
 
   play.addEventListener("click", () => {
+
     if (currentSong.paused) {
+
       currentSong.play();
+
       play.src = "img/pause.svg";
+
     } else {
+
       currentSong.pause();
+
       play.src = "img/play.svg";
+
     }
+
   });
 
   previous.addEventListener("click", () => {
+
     if (currentIndex > 0) {
+
       playMusic(songs[currentIndex - 1]);
+
     }
+
   });
 
   next.addEventListener("click", () => {
+
     if (currentIndex < songs.length - 1) {
+
       playMusic(songs[currentIndex + 1]);
+
     }
+
   });
 
   currentSong.addEventListener("timeupdate", () => {
 
-  let percent = (currentSong.currentTime / currentSong.duration) * 100;
+    let percent = (currentSong.currentTime / currentSong.duration) * 100;
 
-  document.querySelector(".circle").style.left = percent + "%";
+    document.querySelector(".circle").style.left = percent + "%";
 
-  document.querySelector(".songtime").innerHTML =
-    `${secondsToMinutesSeconds(currentSong.currentTime)} / ${secondsToMinutesSeconds(currentSong.duration)}`;
-});
+    document.querySelector(".songtime").innerHTML =
+      `${secondsToMinutesSeconds(currentSong.currentTime)} / ${secondsToMinutesSeconds(currentSong.duration)}`;
+
+  });
 
 }
 
@@ -159,12 +183,15 @@ document.querySelector(".seekbar").addEventListener("click", (e) => {
   let seekbar = document.querySelector(".seekbar");
 
   let rect = seekbar.getBoundingClientRect();
+
   let offsetX = e.clientX - rect.left;
+
   let width = rect.width;
 
   let percent = offsetX / width;
 
   currentSong.currentTime = currentSong.duration * percent;
+
 });
 
 main();
